99a7dd902285fdb07a46add432a9b8118dc58cfb
dstellini <davste93@gmail.com>
2016-08-07 17:51:14 +0200

    Section Management

--- src/app/services/personInfoApiService.ts
+++ src/app/services/personInfoApiService.ts
@@ -1,17 +1,21 @@
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-
-interface PersonInfo {
-  employmentHistory: string[]; // todo
-}
+import { Observable } from 'rxjs';
+import { mockPersonData } from './temporaryData/personInfo';
+import { PersonInfo } from './interfaces';
 
 @Injectable()
 export class PersonInfoApiService {
   constructor(private http: Http) {}
 
+
   find(personId: number) {
     // todo: needs work
-    return this.http.get('/people/' + personId + '/index.json' )
-        .map(res => <PersonInfo[]>res.json());
+    // return this.http.get('/people/' + personId + '/index.json' )
+    //     .map(res => <PersonInfo[]>res.json());
+    // return
+
+    //return
+    Observable.of(new )
   }
 }
